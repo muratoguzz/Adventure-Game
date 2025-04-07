59d107d87054a95315bab3e5015037c2ad262d92
Murat Oğuz <151613636+muratoguzz@users.noreply.github.com>
2025-04-07 22:32:46 +0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        AdventureGame game = new AdventureGame();
+        game.play("road");
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true){
+
+            String direction = scanner.nextLine().trim().toUpperCase().substring(0,1);
+            if(direction.equals("Q")){
+                break;
+            }
+            game.move(direction);
+        }
+
+    }
+}
